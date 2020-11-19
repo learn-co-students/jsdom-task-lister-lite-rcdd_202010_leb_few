198f1f0ae785ef2b810e6a79822b8b345fbf08b9
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-19 10:11:37 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let subButton=document.getElementById("sub-button");
+  let description=document.getElementById("new-task-description");
+  let result=document.getElementById("tasks");
+  
+subButton.addEventListener( 'click', function( event ) {
+result.innerHTML += '<li>' + description.value + ' <button onclick="Delete(this);">x</button> </li>';
+    event.preventDefault();
+  });
+
 });
+
+function Delete(del){
+  del.parentNode.parentNode.removeChild(del.parentNode);
+  }
+
+    
