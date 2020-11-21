a2d3a819f49e51c18b3a960a700992e1c80e19bc
Joseph Tarrazi <joseftaerrazi@gmail.com>
2020-11-21 11:52:39 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("submit",createNewTask);
 });
+const createNewTask=event=>{
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask=document.createElement("li");
+  newTask.innerText= newTaskDescription.value;
+  document.getElementById("tasks").appendChild(newTask);
+  event.target.reset();
+};
