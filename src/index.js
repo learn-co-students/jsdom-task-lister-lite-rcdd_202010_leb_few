875f5d5e2aafa2f512af4f4498684f08d7ad7a45
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-26 16:55:36 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let form = document.queryselector("#create-task-form");
+let list = document.queryselector("#tasks");
+let task = document.queryselector("#new-task-description");
+
+form.addEventListener("submit",function(e) {
+  e.preventdefault();
+  list.innerHTML = `<li> ${task.value} <li>`
+})
 });
