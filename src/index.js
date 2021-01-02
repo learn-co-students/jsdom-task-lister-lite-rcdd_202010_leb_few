1adc5e2a94e376b349cb1ba35a9a19bf98ff879a
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-02 09:59:15 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-  const newTaskUl = document.getElementById("tasks");
+   document.getElementById("create-task-form");
+  document.getElementById("new-task-description");
+  document.getElementById("new-task-priority");
+ document.getElementById("tasks");
   newTaskForm.addEventListener("submit", createNewTask);
 });
+const createNewTask = event => {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+  appendNewTask(newTask);
+  event.target.reset();
+};
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
